ddafd1ca0a1fe152c189ced833dfe82d1f4bf8fa
justzmahesh <53422221+justzmahesh@users.noreply.github.com>
2021-06-19 01:22:49 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -148,10 +148,10 @@ $('document').ready(function(){
 		$('#b3').attr('id','b33')
 		$('#b4').attr('id','b44')
 		$('#b5').attr('id','b55')
-		$('#b11').animate({top:240, left: vw-150},500);
-		$('#b22').animate({top:240, left: vw-50},500);
+		$('#b11').animate({top:240, left: vw+250},500);
+		$('#b22').animate({top:240, left: vw+150},500);
 		$('#b33').animate({top:240, left: vw+50},500);
-		$('#b44').animate({top:240, left: vw+150},500);
+		$('#b44').animate({top:240, left: vw-150},500);
 		$('#b55').animate({top:240, left: vw-250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
